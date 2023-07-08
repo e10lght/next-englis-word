cff3c93f15159e16e49efed6740e84fafd075a63
sw-itok <itok@solutionware.jp>
2023-07-08 14:48:41 +0900

    とりあ

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,7 +1,10 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
 import dayjs from "dayjs";
+import SectionImage from "../../public/images/section.jpg";
+import { NextPage } from "next";
+import Image from "next/image";
 
 type SectionType = {
   name: string;
@@ -11,14 +14,34 @@ type SectionType = {
 
 const Section = (props: SectionType) => {
   const { name, id, created_ad } = props;
+  const getRandomColor = () => {
+    let color = "#";
+    const tmp = (id * 1234567).toString(16);
+    for (let i = 0; i < 6; i++) {
+      color += tmp[i % tmp.length];
+    }
+    return color;
+  };
+
   return (
     <>
       <div>Sections</div>
       <Flex alignItems="center" justifyContent="center">
-        <Link href={{ pathname: "/choice", query: { keyword: id } }}>
-          <Box w="full" borderWidth="1px" borderRadius="lg" py={5} px={55} bg="#f5f5f5">
-            <Text as="b">セクション名：{name}</Text>
-            <Text>追加日：{dayjs(created_ad).format("YYYY年M月DD日")}</Text>
+        <Link href={{ pathname: "/choice", query: { keyword: id } }} style={{ width: "80%" }}>
+          <Box w="full" borderWidth="1px" borderRadius="lg" py={5} px={5} bg="#f5f5f5">
+            <Grid templateRows="repeat(2, 1fr)" templateColumns="repeat(8, 1fr)">
+              <GridItem rowSpan={2} colSpan={1} justifyItems="end">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">
+                  <polygon points="25,0 50,25 25,50 0,25" fill={getRandomColor()} />
+                </svg>
+              </GridItem>
+              <GridItem colSpan={7}>
+                <Text as="b">セクション名：{name}</Text>
+              </GridItem>
+              <GridItem colSpan={7}>
+                <Text>追加日：{dayjs(created_ad).format("YYYY年M月DD日")}</Text>
+              </GridItem>
+            </Grid>
           </Box>
         </Link>
       </Flex>
--- src/pages/choice.tsx
+++ src/pages/choice.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
@@ -17,41 +18,57 @@ type Words = {
   section_id: string;
 };
 
-const Choice = () => {
+type SSRProps = {
+  result: Words[];
+};
+
+const Choice = (props: SSRProps) => {
+  const { result } = props;
   const [sectionWordList, setSectionWordList] = useRecoilState(wordListState);
   const router = useRouter();
   const keyword = Array.isArray(router.query.keyword)
     ? router.query.keyword[0]
     : router.query.keyword;
-  console.log(keyword);
 
   useEffect(() => {
-    const fetchWordList = async (keyword: string) => {
-      console.log(keyword);
-      if (!keyword) return;
-      const { data: resultWordList, error: wordListError } = await supabase
-        .from("wordlist")
-        .select()
-        .eq("section_id", keyword);
-      const result = resultWordList as Words[];
-      setSectionWordList(result);
-    };
-    fetchWordList(keyword!);
+    setSectionWordList(result);
   }, [keyword]);
   return (
     <>
       <div>Choice</div>
       <Link href={{ pathname: "/questions", query: { id: 0 } }}>
+        <br />
         <a>出題ページ</a>
       </Link>
       <Link href={{ pathname: "/itemlist" }}>
+        <br />
         <a>単語一覧</a>
       </Link>
       <Link href={{ pathname: "/review" }}>
+        <br />
         <a>復習ページへ</a>
       </Link>
     </>
   );
+};
+
+// getServerSidePropsはページへのリクエストがある度に実行される
+export const getServerSideProps: GetServerSideProps<SSRProps> = async (context) => {
+  console.log(context.resolvedUrl);
+
+  const query = context.query.keyword;
+  const keyword = Array.isArray(query) ? query[0] : query;
+  const { data: resultWordList, error: wordListError } = await supabase
+    .from("wordlist")
+    .select()
+    .eq("section_id", keyword);
+  const result = resultWordList as Words[];
+
+  return {
+    props: {
+      result,
+    },
+  };
 };
 
 export default Choice;
